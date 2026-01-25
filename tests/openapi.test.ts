62e737af253b99be72fa4b0649f149a9b1763702
Scott Cooper <scttcper@gmail.com>
2026-01-24 22:21:24 -0800

    fix: version

--- tests/openapi.test.ts
+++ tests/openapi.test.ts
@@ -8,7 +8,7 @@ describe('openapi docs', () => {
     const response = await SELF.fetch('http://example.com/api/doc');
     expect(response.status).toBe(200);
     const json = (await response.json()) as OpenApiDocument;
-    expect(json.openapi).toBe('3.0.0');
+    expect(json.openapi).toBe('3.2.0');
     expect(json.info?.title).toBe('SESnoop API');
   });
 });

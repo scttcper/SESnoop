4fb287cac42875c14e0fbc4ef6c37be63d68e1e7
Scott Cooper <scttcper@gmail.com>
2026-06-02 17:02:23 -0700

    test: setup db options correclty

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,28 +1,27 @@
 import path from 'node:path';
 
-import { defineWorkersProject, readD1Migrations } from '@cloudflare/vitest-pool-workers/config';
+import { cloudflareTest, readD1Migrations } from '@cloudflare/vitest-pool-workers';
+import { defineConfig } from 'vitest/config';
 
-export default defineWorkersProject(async () => {
+export default defineConfig(async () => {
   const migrationsPath = path.join(__dirname, 'drizzle');
   const migrations = await readD1Migrations(migrationsPath);
 
   return {
+    plugins: [
+      cloudflareTest({
+        main: './worker/index.ts',
+        miniflare: {
+          compatibilityFlags: ['nodejs_compat'],
+          compatibilityDate: '2025-12-20',
+          d1Databases: ['DB'],
+          bindings: { TEST_MIGRATIONS: migrations, SNS_DISABLE_SIGNATURE_VERIFY: 'true' },
+        },
+      }),
+    ],
     test: {
       setupFiles: ['tests/apply-migrations.ts'],
       globals: true,
-      poolOptions: {
-        workers: {
-          main: './worker/index.ts',
-          singleWorker: true,
-          isolatedStorage: false,
-          miniflare: {
-            compatibilityFlags: ['nodejs_compat'],
-            compatibilityDate: '2025-12-20',
-            d1Databases: ['DB'],
-            bindings: { TEST_MIGRATIONS: migrations, SNS_DISABLE_SIGNATURE_VERIFY: 'true' },
-          },
-        },
-      },
     },
   };
 });
